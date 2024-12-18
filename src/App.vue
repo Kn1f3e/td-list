b513796ef09e7d6bd3734fecae968b65f780af25
Kn1f3e <ic@f0rw0rk.ru>
2024-12-18 17:30:08 +1000

    first

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>Список дел</h1>
+    <input v-model="newTask" @keyup.enter="addTask" placeholder="Добавить новое дело" />
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        {{ task }}
+        <button @click="removeTask(index)">Удалить</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTask: '',
+      tasks: []
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim() !== '') {
+        this.tasks.push(this.newTask.trim());
+        this.newTask = '';
+      }
+    },
+    removeTask(index) {
+      this.tasks.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style>
+/* стили */
+</style>
